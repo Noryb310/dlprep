821c4c8e7b67e33f3727e120b662eadc2a94ac86
Byron Najera <byronmunguia@icloud.com>
2024-10-06 11:35:29 -0700

    Started DL Prep Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome, Negro's React App!</h1>
+        <p>Edit this file to make it your own!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
